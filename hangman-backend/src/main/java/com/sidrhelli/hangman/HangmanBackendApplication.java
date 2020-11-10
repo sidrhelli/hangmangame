ad44bbf4006f24f0fcca3cd8d2fc4d8790ffb066
sergiohellings <sergio.hellings@cronos.nl>
2020-11-10 22:54:35 +0100

    backend

---
+++ hangman-backend/src/main/java/com/sidrhelli/hangman/HangmanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sidrhelli.hangman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangmanBackendApplication.class, args);
+	}
+
+}
